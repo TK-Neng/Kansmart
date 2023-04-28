4e57f451a5164b7bac429b807760620b493355d1
MarkKMUTT <88104343+MarkKMUTT@users.noreply.github.com>
2023-04-28 17:43:52 +0700

    เก่า

---
+++ Front and Back/Front-End/announcement/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Main from './components/Main.vue'
+import Detail from './components/Detail.vue'
+</script>
+
+<template>
+  <!-- <Main/> -->
+  <Detail/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Front and Back/Front-End/announcement/src/components/Main.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+</script>
+
+<template>
+    <dev class="min-h-screen flex flex-col ">
+        <dev class="text-center mt-2 text-3xl font-bold">
+            <h1>SIT Announcement System (SAS)</h1>
+        </dev>
+        <dev class="mt-4 flex flex-row ml-1">
+            <p class=" font-bold">Date/Time shown in Timezone : </p> 
+            <p class=" ml-1">Asia/Bangkok</p>
+        </dev>
+        <dev class="mt-6 ">
+            <table class="table-auto  overflow-hidden flex items-center justify-center border-gray-400 text-lg">
+                <thead>
+                    <tr class="table-row border">
+                        <th class="px-4">No.</th>
+                        <th class="px-4">Title</th>
+                        <th class="px-4">Category</th>
+                        <th class="px-4">Publish Date</th>
+                        <th class="px-4">Close Date</th>
+                        <th class="px-4">Display</th>
+                        <th class="px-4">Action</th>
+                    </tr>
+                    <tr class=" border">
+                        <td class="text-center">1</td>
+                        <td class="whitespace-normal break-all">1fffffffffffffffffffffffdddddddddddddd</td>
+                        <td>1eeeeeeeeeeeeeeeeeeee</td>
+                        <td>1rrrrrrrrrr</td>
+                        <td>1vvvvvvvvvvvvvv</td>
+                        <td>1vvvvvvvvvvvvvvvverv</td>
+                        <td><button class="hover:bg-blue-700 font-bold py-2 px-4 bg-red-500 h-6 w-16 justify-center">View</button></td>
+                    </tr>
+
+                </thead>
+                <tbody></tbody>
+                </table>
+        </dev>
+    </dev>
+</template>
+
+<style scoped>
+</style>
