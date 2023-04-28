11731acd8a13c2f07ca23ee34ee1dcb4ed1ee32c
TECHATHAM KANJANASUTTAKUL <88127892+TK-Neng@users.noreply.github.com>
2023-04-28 15:11:52 +0700

    Delete Back directory

--- Back/src/main/java/com/example/back/Repositories/AnnounceRepositories.java
+++
@@ -1,7 +0,0 @@
-package com.example.back.Repositories;
-
-import com.example.back.Entity.Announces;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnnounceRepositories extends JpaRepository<Announces, Integer> {
-}
