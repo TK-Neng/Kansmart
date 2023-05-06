6a32d23d0d150b7af0e2aeae62bc39d3791c3276
TECHATHAM KANJANASUTTAKUL <88127892+TK-Neng@users.noreply.github.com>
2023-05-06 20:53:23 +0700

    Update index.js

--- Front and Back/Front-End/announcement/src/router/index.js
+++ Front and Back/Front-End/announcement/src/router/index.js
@@ -4,7 +4,7 @@ import Detail from "../views/Detail.vue";
 import Add from "../views/Add.vue";
 import Edit from "../views/Edit.vue";
 
-const history = createWebHistory(import.meta.env.BASE_URL);
+const history = createWebHistory();
 
 const routes = [
   {
