a9120f783c53a06088c64c9d4fa2788149aa068a
TECHATHAM KANJANASUTTAKUL <88127892+TK-Neng@users.noreply.github.com>
2023-05-13 15:36:19 +0700

    Delete Front and Back/Front-End directory

--- Front and Back/Front-End/announcement/cypress.config.cjs
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  e2e: {
-   specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
-   baseUrl:"http://localhost:5173/",
-   experimentalSessionAndOrigin: true
-  },
-});
