e53758c70f507ed791c31902d05b9ec4fde3c8b0
TECHATHAM KANJANASUTTAKUL <88127892+TK-Neng@users.noreply.github.com>
2023-05-14 14:06:35 +0700

    Delete Front and Back/Front-End directory

--- Front and Back/Front-End/announcement/src/stores/announce.js
+++
@@ -1,9 +0,0 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
-export const useAnnounceStore = defineStore('announcer', () => {
-    const mode = ref("active")
-    const category = ref()
-    const page  = ref()
-
-    return {mode , category , page}
-})
